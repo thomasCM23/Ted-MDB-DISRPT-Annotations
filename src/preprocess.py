efbc36bc0b56fb424be685a652cd83d4a0c30f6e
Thomas Chapados Muermans <thomas.chapados.muermans@gmail.com>
2022-02-08 12:14:21 -0500

    Set up environment

---
+++ src/preprocess.py
@@ -0,0 +1,5 @@
+# Preprocess the raw data to create the .conllu, and .tok files, these files will not have discourse connective annotations
+# For .conllu we need to split the sentences and tokenize, this will be done using spacy and conllu packages
+# for .tok we we need to tokenize, this will be done using spacy
+# 
+
